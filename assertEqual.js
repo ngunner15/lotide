21f650a2b9005242e20bd81fcc81fa6f4f1ba6d7
Nisarg <nisargvv@gmail.com>
2020-11-23 19:43:02 +0000

    Adding emojis in console.log

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("🎉🎉🎉Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("⛔⛔⛔Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
