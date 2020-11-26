e10e2eaf8f26a34fd66a3d5515b20de888cd8b0a
Nisarg <nisargvv@gmail.com>
2020-11-26 20:40:02 +0000

    find keys with callback

---
+++ findKey.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(inputObject, callback) {
+  for (const key in inputObject) {
+    if (callback(inputObject[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+const example1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)  // => "noma"
+
+const example2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3)
+
+const example3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 4)
+
+assertEqual(example1, "noma");
+assertEqual(example2, "Akaleri");
+assertEqual(example2, "akaleri");
+assertEqual(example3, undefined);
