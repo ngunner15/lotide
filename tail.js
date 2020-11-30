3116aca8d18c8b1c25dce2fd4389243b58991c2c
Nisarg <nisargvv@gmail.com>
2020-11-30 14:42:31 +0000

    Seperated test cases of tail.js file

--- tail.js
+++ tail.js
@@ -1,12 +1,3 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🎉🎉🎉Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`⛔⛔⛔Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = [];
   for (let i = 0; i < array.length; i++) {
@@ -15,7 +6,4 @@ const tail = function(array) {
   return newArray;
 };
 
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
