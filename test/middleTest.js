11128230808028809ecf944592b12a981ab29067
Nisarg <nisargvv@gmail.com>
2020-11-30 18:19:17 +0000

    Changed middleTest.js to include mocha and chai test framework

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,20 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// TEST CASES
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
-assertArraysEqual(middle([1]), [1]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle(["1", "2", "3"]), ["2"]);
+describe("#middle", () => {
+  it("returns 2 for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [3, 4]) for [1, 2, 3, 4, 5, 6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]); 
+  });
+
+  it("returns [] for [1]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
+
+  it("returns '2' for ['1', '2', '3']", () => {
+    assert.deepEqual(middle(["1", "2", "3"]), ["2"]);
+  });
+});
