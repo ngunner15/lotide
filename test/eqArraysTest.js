37b3d08de84588ab5cd91c7eb13d3d6f816f7eaf
Nisarg <nisargvv@gmail.com>
2020-11-30 20:32:14 +0000

    Changed eqArraysTest.js using mocha and chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,9 +1,24 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["6", "2", "3"]), false);
-assertEqual(eqArrays([], []), true);
+describe("#eqArrays", () => {
+  it("returns true", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+
+  it("returns true", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); 
+  });
+
+  it("returns false", () => {
+    assert.deepEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+  });
+
+  it("returns false", () => {
+    assert.deepEqual(eqArrays(["1", "2", "3"], ["6", "2", "3"]), false);
+  });
+
+  it("returns true", () => {
+    assert.deepEqual(eqArrays([], []), true);
+  });
+});
